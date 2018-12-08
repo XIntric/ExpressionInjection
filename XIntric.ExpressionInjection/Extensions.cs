179e499a7331c59479f909c22dc6b242bd8a4318
Micael Fredriksson <Micael.Fredriksson@sigma.se>
2018-12-08 19:15:53 +0100

    Relocate extension method collection to System.Linq namespace.

--- XIntric.ExpressionInjection/Extensions.cs
+++ XIntric.ExpressionInjection/Extensions.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
+using XIntric.ExpressionInjection;
 
-namespace XIntric.ExpressionInjection
+namespace System.Linq
 {
     public static class ExpressionInjectionExtensions
     {
 
         public static IQueryable<T> EnableInjection<T>(this IQueryable<T> q)
         {
-            return new ExpressionInjection.Queryable<T>(new ExpressionInjection.Provider(q.Provider), q.Expression);
+            return new XIntric.ExpressionInjection.Queryable<T>(new XIntric.ExpressionInjection.Provider(q.Provider), q.Expression);
         }
 
         public static Expression GetExpandedExpression<T>(this IQueryable<T> q)
